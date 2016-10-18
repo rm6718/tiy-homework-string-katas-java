ed6b8b37fbdadd43b79800b43ca337be8fbf1731
Sam <sam@Kellys-MacBook-Air.local>
2016-10-18 15:47:10 -0400

    three

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -12,21 +12,26 @@ public class Kata {
 
 
 
-    //need to figure out how to get rid of the trailing space
 
+    // a public static method that doesn't return anything called explode
     public static void explode(String data){
         System.out.print("Kata.explode(\""+data+"\") -> ");
         System.out.print("\"");
         // work
+
+        //created a reference called letters of type charArray
         char[] letters = data.toCharArray();
        // String delete = data.replaceAll("\\s+$", "");
 
 
-        /*System.out.print(letters.length);*/
 
+        //assign an integer of a value of 0; if the integer number is less than the numbers of letters it will go through the loop;  i++ = incrementing by steps of 1;
         for(int i=0; i<letters.length; i++){
             System.out.print(letters[i]);
-            System.out.print(" ");
+            // as long as the length is not in the last element then print out a space
+            if(letters.length!=i+1) {
+                System.out.print(" ");
+            }
           //  System.out.print(delete);
 
         }
@@ -39,25 +44,24 @@ public class Kata {
 
 
 
-    //this works do not TOUCH
 
     public static void notstring(String data){
         System.out.print("\nKata.notstring(\""+data+"\") -> ");
 
-
-        System.out.print("\"not " +data);
-
+        if (data.startsWith("not")){
+            System.out.print(data);
+        } else {
+            System.out.print("\"not " + data);
+        }
         System.out.print("\"");
     }
 
 
 
 
-    //works, DON'T TOUCH
 
     public static void missingChar(String data, int pos){
         System.out.print("\nKata.missingChar(\""+data+","+pos+"\") -> ");
-
 
         System.out.print(removeCharAt(data, 0));
 
@@ -67,6 +71,21 @@ public class Kata {
     public static String removeCharAt(String s, int pos) {
         return s.substring(0, pos) + s.substring(pos + 1);
     }
+
+
+
+
+
+
+    public static void frontBack(String data){
+        System.out.print("\nKata.frontBack(\'"+data+"\") -> ");
+
+
+
+    }
+
+    }
+
 
 
 
