b7104d17f73878455c168a8d4a548924342730d8
Sam <sam@Kellys-MacBook-Air.local>
2016-10-21 16:40:38 -0400

    nine

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -7,48 +7,42 @@ public class Kata {
         explode("example");
         notstring("happening");
         missingChar("puppy", 3);
+        frontBack("chocolate");
+        frontThree("birthday");
+        backAround("dog");
+        front22("lazy");
+        delDel("adelete");
+        startOz("ozymandias");
+        endUp("hi there");
     }
 
 
-
-
-
     // a public static method that doesn't return anything called explode
-    public static void explode(String data){
-        System.out.print("Kata.explode(\""+data+"\") -> ");
+    public static void explode(String data) {
+        System.out.print("Kata.explode(\"" + data + "\") -> ");
         System.out.print("\"");
         // work
 
         //created a reference called letters of type charArray
         char[] letters = data.toCharArray();
-       // String delete = data.replaceAll("\\s+$", "");
-
 
 
         //assign an integer of a value of 0; if the integer number is less than the numbers of letters it will go through the loop;  i++ = incrementing by steps of 1;
-        for(int i=0; i<letters.length; i++){
+        for (int i = 0; i < letters.length; i++) {
             System.out.print(letters[i]);
             // as long as the length is not in the last element then print out a space
-            if(letters.length!=i+1) {
+            if (letters.length != i + 1) {
                 System.out.print(" ");
             }
-          //  System.out.print(delete);
-
         }
-
         System.out.print("\"");
-
     }
 
 
+    public static void notstring(String data) {
+        System.out.print("\nKata.notstring(\"" + data + "\") -> ");
 
-
-
-
-    public static void notstring(String data){
-        System.out.print("\nKata.notstring(\""+data+"\") -> ");
-
-        if (data.startsWith("not")){
+        if (data.startsWith("not")) {
             System.out.print(data);
         } else {
             System.out.print("\"not " + data);
@@ -57,11 +51,9 @@ public class Kata {
     }
 
 
-
-
-
-    public static void missingChar(String data, int pos){
-        System.out.print("\nKata.missingChar(\""+data+","+pos+"\") -> ");
+    public static void missingChar(String data, int pos) {
+        System.out.print("\nKata.missingChar(\"" + data + "," + pos + "\") -> ");
+        System.out.print("\"");
 
         System.out.print(removeCharAt(data, 0));
 
@@ -73,20 +65,107 @@ public class Kata {
     }
 
 
+    public static void frontBack(String data) {
+        System.out.print("\nKata.frontBack(\"" + data + "\") -> ");
+        System.out.print("\"");
+
+        int fB = data.length();
+
+        if (fB > 1) {
+            System.out.print(data.charAt(fB - 1) + data.substring(1, fB - 1) + data.charAt(0));
+        }
+        System.out.print("\"");
+    }
+
+
+    public static void frontThree(String data) {
+        System.out.print("\nKata.front3(\"" + data + "\") -> ");
+        System.out.print("\"");
+
+        int f3 = data.length();
+
+        if (f3 >= 3) {
+            System.out.print(data.substring(0, 3));
+            System.out.print(data.substring(0, 3));
+            System.out.print(data.substring(0, 3));
+        } else {
+            System.out.print(data);
+        }
+        System.out.print("\"");
+    }
+
+
+    public static void backAround(String data) {
+        System.out.print("\nKata.backAround(\"" + data + "\") -> ");
+        System.out.print("\"");
+
+        int bA = data.length();
+
+        if (bA >= 1) {
+            System.out.print(data.charAt(bA - 1) + data.substring(0, bA) + data.charAt(bA - 1));
+        }
+        System.out.print("\"");
+    }
+
+
+    public static void front22(String data) {
+        System.out.print("\nKata.front22(\"" + data + "\") -> ");
+        System.out.print("\"");
+
+        int f22 = data.length();
+
+        if (f22 >= 2) {
+            System.out.print(data.substring(0, 2) + data.substring(0, f22) + data.substring(0, 2));
+        } else {
+            System.out.print(data.charAt(0) + data.substring(0, f22) + data.charAt(0));
+        }
+        System.out.print("\"");
+    }
+
+
+    public static void delDel(String data) {
+        System.out.print("\nKata.delDel(\"" + data + "\") -> ");
+        System.out.print("\"");
+
+        int dD = data.length();
+
+        if (data.substring(1, 4).equalsIgnoreCase("del")) {
+            System.out.print(data.charAt(0) + data.substring(4, dD));
+        } else {
+            System.out.print(data);
+        }
+        System.out.print("\"");
+    }
 
 
 
+    // not working yet
 
-    public static void frontBack(String data){
-        System.out.print("\nKata.frontBack(\'"+data+"\") -> ");
+    public static void startOz(String data) {
+        System.out.print("\nKata.startOz(\"" + data + "\") -> ");
+        System.out.print("\"");
 
+        int sO = data.length();
 
+        if (sO >= 1 && data.charAt(0) == "o") {
+
+        }
 
     }
 
+
+    public static void endUp(String data) {
+        System.out.print("\nKata.endUp(\"" + data + "\") -> ");
+        System.out.print("\"");
+
+        int eU = data.length();
+        int up = data.length() - 3;
+        String uC = data.substring(up, eU).toUpperCase();
+
+        if (eU >= 3) {
+            System.out.print(data.substring(0, up) + uC);
+        } else {
+            System.out.print(data.toUpperCase());
+        }
     }
-
-
-
-
-    }
+}
